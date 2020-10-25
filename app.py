b69631c3125108e649cc9e9f1e8bc6cbdfb337c3
DKATP <d.katpedraza.9@gmail.com>
2020-10-24 21:45:07 -0500

    Dash1

--- app.py
+++ app.py
@@ -73,5 +73,5 @@ app.layout = html.Div([
            
 
 if __name__ == '__main__':
-    app.run_server(debug = True, 
+    app.run_server(host='0.0.0.0',debug = True, 
                     port = 5000)
